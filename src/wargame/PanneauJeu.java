3d2da3247c18efba3296bfe148ad1abcc0f07a4d
PelouxL <152203841+PelouxL@users.noreply.github.com>
2025-10-28 22:12:26 +0100

    Add files via upload

---
+++ src/wargame/PanneauJeu.java
@@ -0,0 +1,36 @@
+package wargame;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+public class PanneauJeu extends JFrame implements IConfig {
+
+	public  PanneauJeu() {
+		super("Comptez-vous avoir plus de 5 au prochain contrôle de pause café ?");	
+
+		JPanel panneau = new JPanel(new FlowLayout());
+		
+		File imageFile = new File("Wargame/src/wargame/panneauAffiche.gif");
+		System.out.println("Chemin absolu de l'image : " + imageFile.getAbsolutePath());
+		System.out.println("Image existe ? " + imageFile.exists());
+		
+		
+		
+	    ImageIcon img = new ImageIcon("/Wargame/src/wargame/panneauAffiche.gif");
+	    JLabel jLabelImg = new JLabel(img);
+	    panneau.add(jLabelImg);
+	    
+		
+		JLabel labelText = new JLabel("Tu t'es fais Kostiné !");
+		panneau.add(labelText);
+		
+		add(panneau);
+		
+	    pack();
+	    setVisible(true);
+	   }
+
+	  public static void main(String [] args){
+	      JFrame frame = new PanneauJeu();
+	  }
+}
