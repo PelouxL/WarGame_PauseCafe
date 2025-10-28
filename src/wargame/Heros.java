f4c07b1027704f7687563932398c3d4b4d08269b
PelouxL <152203841+PelouxL@users.noreply.github.com>
2025-10-28 12:58:14 +0100

    Add files via upload

---
+++ src/wargame/Heros.java
@@ -0,0 +1,44 @@
+package wargame;
+
+import wargame.ISoldat.TypesH;
+
+public class Heros extends Soldat{
+	private final TypesH TYPE;
+	private final String NOM;
+	private Carte carte;
+	private Position pos;
+	
+	public Heros(Carte carte, TypesH type, String nom, Position pos) {
+		super(carte, type.getPoints(), type.getPortee(), type.getPuissance(), type.getTir(), pos);
+		NOM = nom;
+		TYPE = type;
+	}
+	
+	public int getTour() {
+		return 1; // utiliser une variable pour tour
+	}
+	
+	public int getPoints() {
+		return getPoints();
+	}
+	
+	public int getPortee() {
+		return getPortee();
+	}
+	
+	public void seDeplace(Position newPos) {
+	
+	}
+	
+    public void combat(Soldat soldat) { // dois definir ces fonction pour enlever erreurs
+     
+    }
+
+ 
+    public void joueTour(int tour) { // dois definir ces fonction pour enlever erreurs
+    
+    }
+    
+    
+	
+}
