b991769a5a51a2c67e4216ef3c9eafa631a4c47a
DamienGrjn <damien42.granjon@gmail.com>
2025-11-12 11:31:59 +0100

    Changement fonctions Heros + ajouts Soldats

--- src/wargame/Soldat.java
+++ src/wargame/Soldat.java
@@ -2,13 +2,13 @@ package wargame;
 
 public abstract class Soldat extends Element implements ISoldat{
 	private final int POINT_DE_VIE, PUISSANCE, TIR, PORTEE_VISUELLE;
-	private int pointDeVie;
+	private int pointsDeVie;
 	private Position pos;
 	private Carte carte;
 	private int tour = 1; // 1 = joueur ; 0 = IA
 	
 	Soldat(Carte carte, int pts, int portee, int puiss, int tir, Position pos) {
-		POINT_DE_VIE = pointDeVie = pts;
+		POINT_DE_VIE = pointsDeVie = pts;
 		PORTEE_VISUELLE = portee;
 		PUISSANCE = puiss;
 		TIR = tir;
@@ -16,6 +16,7 @@ public abstract class Soldat extends Element implements ISoldat{
 		this.pos = pos;
 	}
 	
+	// POSITION
 	public Position getPos() {
 		return this.pos;
 	}
@@ -24,7 +25,9 @@ public abstract class Soldat extends Element implements ISoldat{
 		this.pos.setX(pos.getX());
 		this.pos.setY(pos.getY());
 	}
+	// POSITION
 	
+	// TOUR
 	public int getTour() {
 		return this.tour;
 	}
@@ -35,35 +38,54 @@ public abstract class Soldat extends Element implements ISoldat{
 		}
 		this.tour = 1;
 	}
+	
+	public void joueTour(int tour) { // on sait pas ce que ça fait ???
+    	
+    }
+	// TOUR
 
+	// POINTS DE VIE
 	public int getPoints() {
 		return this.POINT_DE_VIE;
 	}
 
 	public int getPointsActuels() {
-		return this.pointDeVie;
+		return this.pointsDeVie;
 	}
 	
+	public void setPointsActuels(int nouveauPoints) {
+		this.pointsDeVie = nouveauPoints;
+	}
+	// POINTS DE VIE
+	
+	// PORTEE
 	public int getPortee() {
 		return this.PORTEE_VISUELLE;
 	}
+	// PORTEE
 	
+	// PUISSANCE
 	public int getPuissance() {
 		return PUISSANCE;
 	}
+	// PUISSANCE
 	
+	// TIR
 	public int getTir() {
 		return TIR;
 	}
+	//TIR
 	
-	public void joueTour(int tour) {
-    	
-    }
-	
+	// COMBAT
 	public void combat(Soldat soldat) {
-	     
+		int degats_joueur = this.getPuissance();
+	    int pv_ennemi = soldat.getPointsActuels();
+	    pv_ennemi -= degats_joueur;
+	    soldat.setPointsActuels(pv_ennemi);
     }
+	// COMBAT
 
+	// DEPLACEMENT
 	public void seDeplace(Position newPos) {
 		if (newPos.estValide()) {
 			this.pos.setX(newPos.getX());
@@ -72,5 +94,6 @@ public abstract class Soldat extends Element implements ISoldat{
 			System.out.println("Erreur seDeplace : position invalide.");
 		}
 	}
+	// DEPLACEMENT
 	
 }
