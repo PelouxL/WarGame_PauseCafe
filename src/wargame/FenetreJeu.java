25eefb8a31640dd5722edd507813fa822ce04011
PelouxL <152203841+PelouxL@users.noreply.github.com>
2025-11-13 14:40:40 +0100

    reajustement affichage

--- src/wargame/FenetreJeu.java
+++ src/wargame/FenetreJeu.java
@@ -1,12 +1,40 @@
 package wargame;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import java.awt.Dimension;
+
 
 public class FenetreJeu {
 
 	public static void main(String[] args) {
-		JFrame frame = new PanneauJeu();
-
+		Carte carte = new Carte();
+		
+		// Creation de la frame
+		JFrame frame = new JFrame("Wargame le 2: La pause café, l'affrontement final");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		/* Modification du JPanel par defaut
+		frame.getContentPane().setBackground(Color.GRAY);
+		frame.getContentPane().setPreferredSize(new Dimension(600, 400)); */
+		
+		// Creation de la JMenuBar
+		JMenuBar GreenMenuBar = new JMenuBar();
+		GreenMenuBar.setOpaque(true);
+		GreenMenuBar.setBackground(Color.green);
+		GreenMenuBar.setPreferredSize(new Dimension(200,100));
+		frame.setJMenuBar(GreenMenuBar);
+		
+		// Ajout du JPanel
+		PanneauJeu PanneauCarte = new PanneauJeu(carte);
+		frame.add(PanneauCarte);
+		
+		// Visibilité de la Jframe
+		frame.pack();
+		frame.setVisible(true);
+		
 	}
 
 }
--- src/wargame/PanneauJeu.java
+++ src/wargame/PanneauJeu.java
@@ -1,5 +1,12 @@
 package wargame;
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import java.awt.Graphics;
 
 import wargame.ISoldat.TypesH;
 import wargame.ISoldat.TypesM;
@@ -8,109 +15,55 @@ import java.awt.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.*;
 
-public class PanneauJeu extends JFrame implements IConfig {
+public class PanneauJeu extends JPanel implements IConfig {
+	private Carte carte;
 	
-	public PanneauJeu() {
-		Carte c = new Carte();
+	
+	public PanneauJeu(Carte c) {
+		this.carte = new Carte();
+		setPreferredSize(new Dimension(LARGEUR_CARTE*NB_PIX_CASE, HAUTEUR_CARTE*NB_PIX_CASE ));
 		
-		JPanel panneau = new JPanel(new BorderLayout());
-		
-		
-		Object[][] donnees = new Object[LARGEUR_CARTE][HAUTEUR_CARTE];  // Matrice d'objets
-	    for (int i = 0; i < LARGEUR_CARTE; i++) {
-	    	for (int j = 0; j < HAUTEUR_CARTE; j++) {
-	    		Element e = c.getElement(new Position(i,j));
-	    		if(e == null) {
-	    			donnees[i][j] = null;
-	    		}else {
-	    			donnees[i][j] = c.getElement(new Position(i,j));  // Remplir la matrice d'objets avec les valeurs de int
-	    		}
-	    	}
-	    }
-	       
-	    String[] colonnes = new String[HAUTEUR_CARTE];
-	    for (int i = 0; i < HAUTEUR_CARTE; i++) {
-	        colonnes[i] = "";
-	    }
-		
-		DefaultTableModel modeleTable = new DefaultTableModel(donnees,colonnes);
-		
-		JTable table = new JTable(modeleTable);
-		
-		// Désactiver l'édition des cellules
-        table.setDefaultEditor(Object.class, null);
-
-        // Désactiver la sélection des cellules
-        table.setCellSelectionEnabled(false);
-        table.setRowSelectionAllowed(false);
-        table.setColumnSelectionAllowed(false);
-		
-        table.getTableHeader().setVisible(false);
-		
-		// couleur pour affichage
-		table.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
+	}
+	
+	public void paintComponent(Graphics g){
+			super.paintComponent(g);
 			
-			public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-				
-				Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-				
-				
-				if (value instanceof Heros) {
-	                 c.setBackground(COULEUR_FORET);
-	            }else if (value instanceof Monstre) {
-	                 c.setBackground(COULEUR_MONSTRES);
-	            } else if (value instanceof Obstacle){
-	            	System.out.println(((Obstacle) value).getType()+" donne le type");
-	            	switch (((Obstacle) value).getType() ) {
-	            		case ROCHER:
-	            			c.setBackground(COULEUR_ROCHER);
-	            			break;
-	            		case EAU:
-	            			c.setBackground(COULEUR_EAU);
-	            			break;
-	            		case FORET:
-	            			c.setBackground(COULEUR_FORET);
-	            			break;
-	            	}
-	            }else {
-	            	c.setBackground(COULEUR_VIDE);
-	                
-	            }
-				setText("");
-			return c;
-			
+			for(int i = 0; i < LARGEUR_CARTE; i++) {
+				for(int j = 0; j < HAUTEUR_CARTE; j++) {
+					
+					Element e = carte.getElement(new Position(i,j));
+					if(e instanceof Heros) {
+						g.setColor(COULEUR_HEROS);
+					}else if(e instanceof Monstre) {
+						g.setColor(COULEUR_MONSTRES);
+					}else if(e instanceof Obstacle) {
+						switch(((Obstacle)e).getType()) {
+						case ROCHER:
+							g.setColor(COULEUR_ROCHER);
+							break;
+						case EAU:
+							g.setColor(COULEUR_EAU);
+							break;
+						case FORET:
+							g.setColor(COULEUR_FORET);
+							break;
+						}
+					}else {
+						g.setColor(COULEUR_VIDE);
+						
+					}
+					g.fillRect(i*NB_PIX_CASE, j*NB_PIX_CASE, NB_PIX_CASE, NB_PIX_CASE);
+					
+					// Bord de la case
+					g.setColor(Color.BLACK);
+					g.drawRect(i*NB_PIX_CASE, j*NB_PIX_CASE, NB_PIX_CASE, NB_PIX_CASE);
+				}
 			}
-				
-		});
-		
-		table.setRowHeight(NB_PIX_CASE); // hauteur des cases
-        table.setGridColor(Color.GRAY);
-		
-		JScrollPane scrollPane = new JScrollPane(table);
-		
-		JPanel panelHaut = new JPanel();
-	    panelHaut.setLayout(new FlowLayout(FlowLayout.CENTER));
 
-		JButton labelBouton = new JButton("Fin de Tour");
-		panelHaut.add(labelBouton);
-		
-		JLabel labelText = new JLabel("Voici le bouton fin de tour");
-		panelHaut.add(labelText);
-		
-		panelHaut.setAlignmentX(Component.CENTER_ALIGNMENT);
-		
-		panneau.add(scrollPane, BorderLayout.CENTER);  // Placer la table au centre
-        panneau.add(panelHaut, BorderLayout.NORTH); 
-		
-		JFrame fenetre = new JFrame("Comptez-vous avoir plus de 5 au prochain contrôle de pause café ?");
+	}
+	
 		
 		
-		fenetre.add(panneau);
-		fenetre.setMinimumSize(new java.awt.Dimension(1000,600));
-		fenetre.pack();
-		fenetre.setLocationRelativeTo(null);
-		fenetre.setVisible(true);
-	   }
+}
 	      
 
-}
